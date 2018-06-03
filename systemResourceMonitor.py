f5f99e0edd9a25bc0c6fd8d856e8eac67bb00264
Ian Tsai <b10102016@gmail.com>
2018-06-03 22:50:03 +0800

    debug

--- systemResourceMonitor.py
+++ systemResourceMonitor.py
@@ -2,7 +2,7 @@ import utilitys
 import os
 
 os.system("adb -s 192.168.0.10 shell am start com.android.chrome/com.google.android.apps.chrome.Main -d https://stackoverflow.com/")
-utilitys.dumpsys_gfxinfo("192.168.0.10","com.android.chrome","com.android.chrome/com.google.android.apps.chrome.Main -d https://stackoverflow.com/")
+utilitys.dumpsys_gfxinfo("192.168.0.10","com.android.chrome","com.google.android.apps.chrome.Main -d https://stackoverflow.com/")
 
 
 
