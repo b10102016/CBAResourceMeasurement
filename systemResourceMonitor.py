4534cb523a28dd31521acffb4d387820113d1d97
Ian Tsai <b10102016@gmail.com>
2018-06-03 23:09:34 +0800

    testing

--- systemResourceMonitor.py
+++ systemResourceMonitor.py
@@ -1,8 +1,7 @@
 import utilitys
 import os
 
-os.system("adb -s 192.168.0.10 shell am start com.android.chrome/com.google.android.apps.chrome.Main -d https://stackoverflow.com/")
-utilitys.dumpsys_gfxinfo("192.168.0.10","com.android.chrome","com.google.android.apps.chrome.Main -d https://stackoverflow.com/")
+utilitys.dumpsys_gfxinfo("192.168.0.10","com.tyy.recyclerview","com.tyy.recyclerview.MainActivity")
 
 
 
