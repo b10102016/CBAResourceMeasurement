52eab9b31643aaa58358b7df07fc31095f0395eb
Ian Tsai <b10102016@gmail.com>
2018-06-04 13:59:22 +0800

    push

--- systemResourceMonitor.py
+++ systemResourceMonitor.py
@@ -1,7 +1,7 @@
 import utilitys
 import os
 import GPUMonitor
-GPUMonitor.dumpsys_gfxinfo("192.168.0.10","com.example.android.recyclerview",".MainActivity")
+GPUMonitor.dumpsys_gfxinfo("192.168.0.10","xyz.bluephoenix.stresscpu",".MainActivity")
 
 
 
