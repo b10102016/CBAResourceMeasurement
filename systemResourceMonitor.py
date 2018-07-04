85f187902c4f9f37606077208e07848d7e1a6b56
Ian Tsai <b10102016@gmail.com>
2018-07-04 09:33:53 +0800

    update data

--- systemResourceMonitor.py
+++ systemResourceMonitor.py
@@ -20,8 +20,8 @@ MAX_NODE_IN_CLUSTER=35
 from openpyxl import Workbook
 
 wb= Workbook()
-ws=wb.active
-ws.title="statics"
+
+
 for offset in range(0,MAX_NODE_IN_CLUSTER):
     ip=ip_prefix+str(start_ip+offset)
     thd = threading.Thread(target=GPUMonitor.dumpsys_gfxinfo,args=(ip,"","",10,wb,))
@@ -34,8 +34,19 @@ for thd in thd_pool:
     while thd.is_alive():
         time.sleep(0.5)
 
+ws = wb.create_sheet(title="statics")
+_1stSheetName = wb.worksheets[0].title
+_2ndSheetName = wb.worksheets[-1].title
+# Generate Cluster Statics
+ws['A1']="Avg>16msCnt"
+ws['A2']="SUMPRODUCT(COUNTIF(INDIRECT(\"'result-192.168.0.\"&ROW(%d:%d)&\"'!G2\"),\">16\"))"%(start_ip,MAX_NODE_IN_CLUSTER+start_ip-1)
+ws['B1']=">100>16msCnt"
+ws['B2']="SUMPRODUCT(COUNTIF(INDIRECT(\"'result-192.168.0.\"&ROW(%d:%d)&\"'!G4\"),\">100\"))"%(start_ip,MAX_NODE_IN_CLUSTER+start_ip-1)
+ws['C1']=">50>32msCnt"
+ws['C2']="SUMPRODUCT(COUNTIF(INDIRECT(\"'result-192.168.0.\"&ROW(%d:%d)&\"'!H4\"),\">50\"))"%(start_ip,MAX_NODE_IN_CLUSTER+start_ip-1)
 
 fileName="ClusterFPS_Statics.xlsx"
+
 wb.save(fileName)
 
 
